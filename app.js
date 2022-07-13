236b9610eced5c68cc0a21c596a719c9ae46d146
Mariam <mariam.dateshidze.1@btu.edu.ge>
2022-07-13 12:03:34 +0400

    add new js file

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const fixedBtn = document.querySelector("#fixed-btn");
+const moreBtn = document.querySelector(".moreinfo-button");
+const moreInfo = document.querySelector(".more-info");
+const sendInfo = document.querySelector(".send-button");
+
+
+let activeIndex = 0;
 
 fixedBtn.addEventListener('click', () => {
     window.scrollTo({ top: 0, left: 100, behavior: 'smooth' });
-  })
+  });
+
+moreBtn.addEventListener('click', () => {
+    moreInfo.classList.toggle('active')
+    });
+
+sendInfo.addEventListener('click', () => {
+    alert("თქვენი ინფორმაცია გაიგზავნა");
+    });
+
+
+
+
+
+
---
+++ second.js
@@ -0,0 +1,10 @@
+const sendInfoSecond = document.querySelector("#secondsend");
+const fixedBtn = document.querySelector("#fixed-btn");
+
+fixedBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, left: 100, behavior: 'smooth' });
+  });
+
+sendInfoSecond.addEventListener('click', () => {
+    alert("თქვენი ინფორმაცია გაიგზავნა");
+    });
